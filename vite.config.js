fc17dcf941905a135a6125f1426a5974f743e27f
James Lyne <jim+github@not-null.co.uk>
2022-03-23 21:02:08 +0000

    Only use vite.config.json for production builds

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import laravel from 'vite-plugin-laravel'
 
-export default defineConfig({
-  plugins: [
-    laravel({
-        config: 'vite.config.json',
-    })
-  ]
-})
+export default ({mode}) => {
+    return defineConfig({
+        server: {
+            host: '0.0.0.0'
+        },
+        plugins: [
+            laravel(mode === 'production' ? {
+                config: 'vite.config.json',
+            } : undefined)
+        ]
+    });
+};
